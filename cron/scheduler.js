667e7975a3396e18dfd7e72fcca7e1f2a8aa1537
shairya <sumit.shairya@gmail.com>
2025-11-02 19:11:12 +0530

    cron stopped

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -3,17 +3,17 @@ const cron = require("node-cron");
 const { Op } = require("sequelize");
 const Article = require("../models/Article");
 
-cron.schedule("*/30 * * * *", async () => {
-  const now = new Date();
-  const articles = await Article.findAll({
-    where: {
-      status: "draft",
-      scheduledPublishDate: { [Op.lte]: now },
-    },
-  });
+// cron.schedule("*/30 * * * *", async () => {
+//   const now = new Date();
+//   const articles = await Article.findAll({
+//     where: {
+//       status: "draft",
+//       scheduledPublishDate: { [Op.lte]: now },
+//     },
+//   });
 
-  for (const article of articles) {
-    await article.update({ status: "published" });
-    console.log(`Published scheduled article: ${article.title}`);
-  }
-});
+//   for (const article of articles) {
+//     await article.update({ status: "published" });
+//     console.log(`Published scheduled article: ${article.title}`);
+//   }
+// });
