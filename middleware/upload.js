53edf0a494ff974781e07ccf2c02e78767756279
shairya <sumit.shairya@gmail.com>
2025-11-10 11:01:21 +0530

    category bulk upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -25,4 +25,35 @@ const createUploadMiddleware = (type = "default") => {
   return multer({ storage });
 };
 
+const uploadCsv = (type = "default") => {
+  const storage = multer.diskStorage({
+    limits: { fileSize: 5 * 1024 * 1024 },
+    destination: function (req, file, cb) {
+      const category = req.body.name || "default";
+      const uploadPath = path.join(
+        __dirname,
+        "../public/uploads/csv",
+        type,
+        category
+      );
+
+      fs.mkdirSync(uploadPath, { recursive: true });
+      cb(null, uploadPath);
+    },
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype === "text/csv" || file.originalname.endsWith(".csv")) {
+        cb(null, true);
+      } else {
+        cb(new Error("Only CSV files are allowed"));
+      }
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+
+  return multer({ storage });
+};
+
 module.exports = createUploadMiddleware;
+module.exports = uploadCsv;
